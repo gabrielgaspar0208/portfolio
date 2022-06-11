99309fef12a59dda5324f0f7498851ad2d41d3d6
unknown <gabrielgaspar0208@gmail.com>
2022-06-11 03:01:14 -0300

    change: criado componente project.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,36 @@
-import Slide from '../components/slide/Slide';
 import '../styles/projects.scss';
-
+import letmeaskProject from '../assets/images/letmeask.png';
 
 function Projects() {
   return (
     <section id="projects">
-      <h2>Alguns Projetos</h2>
-        <div>
-          <Slide />
+      <h1>Alguns Projetos</h1>
+      <div className="list">
+      <div className="list-item">
+          <a href="https://letmeask.com.br" target="_blank">           
+            <img src={letmeaskProject}alt="" />
+             <h5>2021</h5>
+             <h2>Letmeask - Rocketseat</h2>        
+             <p>Ver Projeto &raquo;&raquo;</p>         
+          </a>
         </div>
+        <div className="list-item">
+          <a href="https://letmeask.com.br" target="_blank">           
+            <img src={letmeaskProject}alt="" />
+             <h5>2021</h5>
+             <h2>Letmeask - Rocketseat</h2>        
+             <p>Ver Projeto &raquo;&raquo;</p>         
+          </a>
+        </div>
+        <div className="list-item">
+          <a href="https://letmeask.com.br" target="_blank">           
+            <img src={letmeaskProject}alt="" />
+             <h5>2021</h5>
+             <h2>Letmeask - Rocketseat</h2>        
+             <p>Ver Projeto &raquo;&raquo;</p>         
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
